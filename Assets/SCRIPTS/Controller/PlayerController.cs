72515a8c9e2baa9bb96654e077da6f9971711e37
Dexter Estrada <dexter.estrada99@gmail.com>
2022-10-25 16:44:05 -0700

    Added basic PlayerController

--- Assets/SCRIPTS/Controller/PlayerController.cs
+++ Assets/SCRIPTS/Controller/PlayerController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ *  A class to be attached to the player object. Controls overworld player
+ */
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,6 +16,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // Player goes up
+            Debug.Log("Player is going up");
+        }
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Player goes left
+            Debug.Log("Player is going left");
+        }
+        if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            // Player goes down
+            Debug.Log("Player is going down");
+        }
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Player goes right
+            Debug.Log("Player is going right");
+        }
     }
 }
