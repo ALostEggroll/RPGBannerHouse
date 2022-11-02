87e2ad836d28ab9ee930dcb493241bc453ae8d91
daphned1 <92963958+daphned1@users.noreply.github.com>
2022-11-02 02:26:06 -0700

    Sprite transition from idle to run
    - Sprite transition from idle to run using keys
    - Sprite flips horizontally when moving left to right

--- Assets/SCRIPTS/Controller/PlayerController.cs
+++ Assets/SCRIPTS/Controller/PlayerController.cs
@@ -8,9 +8,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;    // Move speed
+    bool facingRight = true;
 
     //public Rigidbody rb;    // Player model
     public CharacterController controller;
+    public Animator animator;
 
     Vector3 movement;   // Player movement in the world
 
@@ -20,8 +22,21 @@ public class PlayerController : MonoBehaviour
         // Player movement in the x(left and right) direction. Covers A, D, Left key, Right key
         movement.x = Input.GetAxisRaw("Horizontal");
 
+        // Changes sprite based on speed moving horizontally
+        animator.SetFloat("Speed", Mathf.Abs(movement.x));
+
         // Player movement in the z(forward and backward) direction. Covers W, S, Up key, Down key
         movement.z = Input.GetAxisRaw("Vertical");
+
+        // Flip character
+        if (movement.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        if (movement.x < 0 && facingRight)
+        {
+            Flip();
+        }
 
         // Updates the player model
         controller.SimpleMove(controller.center + movement * moveSpeed);
@@ -31,5 +46,15 @@ public class PlayerController : MonoBehaviour
     {
         // Updates the player model
         //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    // Controls flipping of character
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
