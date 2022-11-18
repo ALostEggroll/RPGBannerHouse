5db35693ddf497117c0c715bbf9a46436a6b11b3
Dexter Estrada <dexter.estrada99@gmail.com>
2022-11-17 20:00:38 -0800

    Swapped character controller with rigidbody

--- Assets/SCRIPTS/Controller/PlayerController.cs
+++ Assets/SCRIPTS/Controller/PlayerController.cs
@@ -10,14 +10,44 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;    // Move speed
     bool facingRight = true;
 
-    //public Rigidbody rb;    // Player model
-    public CharacterController controller;
+    public Rigidbody rb;    // Player model
+    //public CharacterController controller;
     public Animator animator;
 
     Vector3 movement;   // Player movement in the world
 
     // Update is called once per frame
     void Update()
+    {
+        // Player movement in the x(left and right) direction. Covers A, D, Left key, Right key
+        //movement.x = Input.GetAxisRaw("Horizontal");
+
+        // Changes sprite based on speed moving horizontally
+        //animator.SetFloat("Speed", Mathf.Abs(movement.x));
+
+        // Player movement in the z(forward and backward) direction. Covers W, S, Up key, Down key
+       //movement.z = Input.GetAxisRaw("Vertical");
+
+        // Flip character
+        //if (movement.x > 0 && !facingRight)
+        //{
+            //Flip();
+        //}
+        //if (movement.x < 0 && facingRight)
+        //{
+            //Flip();
+        //}
+
+        // Updates the player model
+        //controller.SimpleMove(movement * moveSpeed);
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
     {
         // Player movement in the x(left and right) direction. Covers A, D, Left key, Right key
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -38,14 +68,10 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
 
-        // Updates the player model
-        controller.SimpleMove(controller.center + movement * moveSpeed);
-    }
-
-    void FixedUpdate()
-    {
+        movement.y = rb.velocity.y;
         // Updates the player model
         //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = new Vector3(movement.x * moveSpeed, movement.y, movement.z * moveSpeed);
     }
 
     // Controls flipping of character
