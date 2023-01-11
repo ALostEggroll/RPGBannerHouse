7e3434ccebb62aa81237b618bb261a4e624e53c6
Dexter Estrada <dexter.estrada99@gmail.com>
2023-01-10 19:42:25 -0800

    Added inventory script

---
+++ Assets/SCRIPTS/Inventory.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> Items = new List<Item>();
+
+    public void addItem()
+    {
+
+    }
+    
+    public void removeItem()
+    {
+
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
